efeaedd600930dd3bccc47da75898fa993f52a52
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-09 14:54:34 +0700

    Remove colon from each labels

--- src/components/message-view.js
+++ src/components/message-view.js
@@ -8,15 +8,15 @@ class MessageView extends React.Component {
     return (
       <div className="message">
         <div className="field">
-          <span className="label">From: </span>
+          <span className="label">From</span>
           <span className="value">{message.from}</span>
         </div>
         <div className="field">
-          <span className="label">Status: </span>
+          <span className="label">Status</span>
           <span className="value">{message.status}</span>
         </div>
         <div className="field">
-          <span className="label">Message: </span>
+          <span className="label">Message</span>
           <span className="value">{message.content}</span>
         </div>
       </div>
