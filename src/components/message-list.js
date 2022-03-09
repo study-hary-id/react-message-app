6f42d06593077f559c64920affd74fcbe3167e18
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-09 14:55:42 +0700

    Update the state and iterate over the array

--- src/components/message-list.js
+++ src/components/message-list.js
@@ -3,18 +3,37 @@ import MessageView from "./message-view";
 
 class MessageList extends React.Component {
   state = {
-    message: {
-      from: "John Doe",
-      status: "unread",
-      content: "Have a nice day!"
-    }
-  }
+    messages: [
+      {
+        _id: 'd2504a54',
+        from: 'John',
+        content: 'The event will start next week',
+        status: 'unread'
+      },
+      {
+        _id: 'fc7cad74',
+        from: 'Martha',
+        content: 'I will be traveling soon',
+        status: 'read'
+      },
+      {
+        _id: '876ae642',
+        from: 'Jacob',
+        content: 'Talk later. Have a great day!',
+        status: 'read'
+      }
+    ]
+  };
 
   render() {
+    const messageViews = this.state.messages.map(
+      message => <MessageView key={message._id} message={message} />
+    );
+
     return (
       <div className="container">
         <h1>List of Messages</h1>
-        <MessageView message={this.state.message} />
+        {messageViews}
       </div>
     );
   }
