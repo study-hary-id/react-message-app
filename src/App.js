9de25a4a087452cc743a477bb5c721d787f7709b
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-12 15:20:36 +0700

    Import Login to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import './App.css';
+import Login from "./components/pages/login";
 import MessageList from "./components/message-list";
 
 function App() {
   return (
-    <MessageList />
+    <Login />
   );
 }
 
